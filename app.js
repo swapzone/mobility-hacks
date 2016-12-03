3c5a8b6df6a455651dfa57ab05c20c50ae480b31
Sascha Gros <gros.sascha@gmail.com>
2016-12-03 16:04:18 +0100

    Implement basic message processing call

--- app.js
+++ app.js
@@ -30,6 +30,48 @@ app.get('/webhook', function(req, res) {
 	}
 });
 
+/*
+ * The actual message processing callback url.
+ */
+app.post('/webhook', function (req, res) {
+	let data = req.body;
+
+	// is it a page subscription?
+	if (data.object == 'page') {
+		// there might be multiple entries if batched by Facebook
+		data.entry.forEach(function(pageEntry) {
+			let pageId = pageEntry.id;
+			let timeOfEvent = pageEntry.time;
+
+			console.log(pageId);
+			console.log(timeOfEvent);
+
+			// iterate over each messaging event
+			pageEntry.messaging.forEach(messagingEvent => {
+				if (messagingEvent.optin) {
+					console.log('Received authentication event.');
+				} else if (messagingEvent.message) {
+					console.log('Received message event.');
+				} else if (messagingEvent.delivery) {
+					console.log('Received message delivery event.');
+				} else if (messagingEvent.postback) {
+					console.log('Received message postback event.');
+				} else if (messagingEvent.read) {
+					console.log('Received message read event.');
+				} else {
+					console.log("Webhook received unknown messagingEvent.");
+				}
+
+				console.log(messagingEvent);
+			});
+		});
+
+		// You must send back a 200, within 20 seconds, to let us know you've
+		// successfully received the callback. Otherwise, the request will time out.
+		res.sendStatus(200);
+	}
+});
+
 // start server
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
