4cd058d094e4102ae36240bceba9dd6f992cf505
Sascha Gros <gros.sascha@gmail.com>
2016-12-03 15:50:42 +0100

    Add initial webhook call

--- app.js
+++ app.js
@@ -5,12 +5,29 @@ let express = require('express'),
 
 let app = express();
 
+const VALIDATION_TOKEN = 'VerySecretToken';
+
 app.set('port', process.env.PORT || 8000);
 app.use(express.static('public'));
 
-app.get('*', function(req, res) {
+app.get('*', function(req, res, next) {
 	console.log('New request coming in...');
-	res.sendStatus(200);
+	next();
+});
+
+/*
+ * Evaluation endpoint for Facebook to check if our application is the real
+ * one.
+ */
+app.get('/webhook', function(req, res) {
+	if (req.query['hub.mode'] === 'subscribe' &&
+		req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+		console.log('Validating validation token...');
+		res.status(200).send(req.query['hub.challenge']);
+	} else {
+		console.error('Validation failed. Make sure you have a correct validation token.');
+		res.sendStatus(403);
+	}
 });
 
 // start server
